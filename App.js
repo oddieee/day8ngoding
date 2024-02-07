0d2719a03a0f583f122f3d9af654b18ee65c25e4
Satria Abimayu <satriaabimayu01@gmail.com>
2024-02-07 10:14:29 +0700

    userlist

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import React from 'react';
+import {SafeAreaView, StatusBar} from 'react-native';
+import UserList from './src/components/UserList';
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+    <StatusBar barStyle="dark-content" />
+    <SafeAreaView style={{flex:1}}>
+      <UserList />
+    </SafeAreaView>
+    </>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+};
---
+++ src/screens/UserList.js
@@ -0,0 +1,63 @@
+import React, {useState, useEffect} from 'react';
+import {View, Text, Flatlist, Stylesheet} from 'react-native';
+import axios from 'axios';
+
+const UserList = () =>{
+    const [users, setUsers] = useState([]);
+
+    useEffect (()=>{
+        const fetchData = async () => {
+            try{
+                const response = await axios.get('https://jsonplaceholder.typicode.com/');
+                setUsers(response.data);
+            }catch(error){
+                console.error('Error fetching data:', error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>List of Users</Text>
+            <Flatlist
+            data={users}
+            keyExtractor={item => String(item.id)}
+            renderItem={({item}) => (
+                <View style={styles.userItem}>
+                    <Text style={styles.userName}>{item.name}</Text>
+                    <Text>Email: {item.email}</Text>
+                    <Text>Phone: {item.phone}</Text>
+                </View>
+            )}   
+            />         
+        </View>
+    );
+};
+
+const styles = Stylesheet.create({
+    container:{
+        flex:1,
+        padding:16,
+    },
+    title:{
+        fontSize:20,
+        fontWeight:'bold',
+        marginBottom: 16,
+    },
+    userItem:{
+        marginBottom:16,
+        borderWidth:1,
+        borderColor:'#ddd',
+        padding:10,
+        borderRadius:8,
+    },
+    userName:{
+        fontSize:16,
+        fontWeight:'bold',
+        marginBottom:8,
+    },
+});
+
+export default UserList;
